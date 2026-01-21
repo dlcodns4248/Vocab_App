82eb529d8e5528312dda6b4156d037d402ac9c15
Hye Seok <hasuk211@naver.com>
2026-01-22 05:00:46 +0900

    단어장 등록 이동과 recyclerview item 레이아웃 수정

--- app/src/main/java/com/example/vocaapp/VocabularyListAdapter.java
+++ app/src/main/java/com/example/vocaapp/VocabularyListAdapter.java
@@ -18,6 +18,7 @@ public class VocabularyListAdapter extends RecyclerView.Adapter<VocabularyListAd
         this.dataList = dataList;
     }
 
+    // 각 아이템 뷰를 보관하고, 내부 위젯(TextView 등)에 접근할 수 있게 하는 ViewHolder
     public static class VocabularyListViewHolder extends RecyclerView.ViewHolder {
         TextView textViewItem;
 
@@ -27,6 +28,7 @@ public class VocabularyListAdapter extends RecyclerView.Adapter<VocabularyListAd
         }
     }
 
+    // 아이템 뷰를 생성하고 ViewHolder를 반환
     @NonNull
     @Override
     public VocabularyListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -35,12 +37,14 @@ public class VocabularyListAdapter extends RecyclerView.Adapter<VocabularyListAd
         return new VocabularyListViewHolder(view);
     }
 
+    // ViewHolder의 뷰에 데이터를 설정
     @Override
     public void onBindViewHolder(@NonNull VocabularyListViewHolder holder, int position) {
         String data = dataList.get(position);
         holder.textViewItem.setText(data);
     }
 
+    // 데이터 리스트의 크기를 반환
     @Override
     public int getItemCount() {
         return dataList.size();
--- app/src/main/java/com/example/vocaapp/VocabularyListFragment.java
+++ app/src/main/java/com/example/vocaapp/VocabularyListFragment.java
@@ -1,13 +1,17 @@
 package com.example.vocaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,6 +24,7 @@ public class VocabularyListFragment extends Fragment {
     private VocabularyListAdapter adapter;
     private final List<String> dataList = new ArrayList<>();
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -28,8 +33,18 @@ public class VocabularyListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_vocabulary_list, container, false);
 
         recyclerView = view.findViewById(R.id.recyclerViewVocabulary);
+        ImageView vocaRegisterImageView = view.findViewById(R.id.vocaRegisterImageView);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        vocaRegisterImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(requireContext(), VocabularyRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
 
         // 3. 데이터 준비
         for (int i = 1; i <= 20; i++) {
